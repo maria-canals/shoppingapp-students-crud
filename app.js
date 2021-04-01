e84ee4370bec6c1df7e9cb31ff783273e4a66aaf
KevinSalcedoUAB <kevinarmando.salcedo@e-campus.uab.cat>
2021-04-01 13:33:01 +0200

    5 done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 //Product Constructor
 class Product {
-  constructor(name, price, year) {
+  constructor(id, name, price, year) {
+    this.id = id;
     this.name = name;
     this.price = price;
     this.year = year;
@@ -12,6 +13,7 @@ class Product {
 class UI {
   //Product template
   static addProduct(product) {
+    console.log("producto: ", product)
     const productList = document.getElementById("product-list");
     const element = document.createElement("div");
     element.innerHTML = `
@@ -20,7 +22,7 @@ class UI {
       <h5><strong>${product.name}</strong></h5>
       <strong>Price</strong>: ${product.price}€
       <strong>Year</strong>: ${product.year}
-      <a href="#" onclick="UI.deleteProduct(event)" class="dlt btn btn-danger ml-5" name="delete">Delete</a>
+      <a href="#" id="${product.id}" onclick="UI.deleteProduct(event)" class="dlt btn btn-danger ml-5" name="delete">Delete</a>
       </div>
       </div>
       `;
@@ -32,8 +34,15 @@ class UI {
   }
 
   static deleteProduct(event) {
-    console.log("event", event)
+
     event.target.closest("div.card.text-center.mb-4").remove();
+
+    fetch(`http://ec2-35-181-5-201.eu-west-3.compute.amazonaws.com:8080/delete-product/ants/${event.target.id}`, {
+      method: 'GET',
+    })
+    .then(res => res.text()) // or res.json()
+    .then(res => console.log(res))
+
     UI.showMessage("Product removed successfully", "danger");
   }
 
@@ -61,7 +70,7 @@ class UI {
       then(data => {
         data.forEach(product => {
           if (product.year < 2021) {
-            const prdct = new Product(product.title, product.price, product.year);
+            const prdct = new Product(product.id, product.title, product.price, product.year);
             UI.addProduct(prdct);
           }
         })
@@ -76,14 +85,13 @@ class UI {
   // }
 
   static sendProducts(product) {
+    //console.log("producto send: ", product)
     let prd = JSON.stringify({
-      'id': Math.floor(Math.random() * 10000),
       'title': product.name,
       'price': product.price,
       'year': product.year
     })
 
-    console.log(typeof prd, prd)
     const response = fetch('http://ec2-35-181-5-201.eu-west-3.compute.amazonaws.com:8080/add-product/ants', {
       method: 'POST',
       headers: {
@@ -104,11 +112,17 @@ document.getElementById("product-form").addEventListener("submit", e => {
   year = document.getElementById("product-year").value
 
   //Save product
-  const product = new Product(name, price, year);
+  const product = new Product(id, name, price, year);
   UI.sendProducts(product)
   UI.addProduct(product);
-  UI.resetForm();
+  //UI.resetForm();
   UI.showMessage("Product added successfully", "success");
 
   e.preventDefault();
+});
+
+document.getElementById("product-name").addEventListener("input", e=> {
+  if(e.target.value.length==20){
+    UI.showMessage("Mucho texto", "danger");
+  }
 });
